b2efa438eaef503eda89f53106dc857fdd044006
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2020-09-15 09:59:22 +0200

    flash messages

--- src/context/FlashMessagesContext.js
+++ src/context/FlashMessagesContext.js
@@ -1,4 +1,4 @@
-import React, {useState, createContext, useContext, useEffect, useRef} from 'react';
+import React, {useState, createContext, useContext} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
 export const FlashMessagesContext = createContext({
@@ -10,51 +10,44 @@ export const FlashMessagesContext = createContext({
 const FlashMessagesContextProvider = ({children}) => {
     const [messages, setMessages] = useState([]);
 
-    // useEffect(() => {
-    //
-    //
-    //     removeIfAutoClose();
-    // }, [messages]);
+        async function addFlashMessage(type, title, description = null, autoClosed = true, timeClosed = 2000) {
+            const flashMessage = {
+                uuid: uuidv4(),
+                type: type,
+                title: title,
+                description: description,
+                autoClosed: autoClosed,
+                timeClosed: timeClosed,
+                status: true
+            };
 
-    async function addFlashMessage(type, title, description = null, autoClosed = true, timeClosed = 2000) {
-        const flashMessage = {
-            uuid: uuidv4(),
-            type: type,
-            title: title,
-            description: description,
-            autoClosed: autoClosed,
-            timeClosed: timeClosed,
-            status: true
-        };
+            setMessages([flashMessage].concat(messages));
 
-        await setMessages([flashMessage, ...messages]);
+            await setTimeout(() => {
+                const msgs = messages;
+                setMessages(msgs => msgs.filter(msg => msg !== flashMessage));
+            }, timeClosed);
+        }
+
+        async function removeMessage(msg) {
+            const update = await messages.filter(item => item !== msg)
+            await setMessages(update);
+        }
+
+        return (
+            <FlashMessagesContext.Provider value={{
+                messages,
+                addFlashMessage,
+                removeMessage
+            }}>
+                {children}
+            </FlashMessagesContext.Provider>
+        )
+    };
+    export default FlashMessagesContextProvider;
+
+    export function useFlashMessages() {
+        return useContext(FlashMessagesContext);
     }
-
-    async function removeMessage(msg) {
-        const update = await messages.filter(item => item !== msg)
-        // const index = messages.findIndex(item => item.uuid === msg.uuid);
-        // const msgs = [...messages];
-        // msgs[]
-
-        console.log('update ', update.length)
-        await setMessages(update);
-    }
-
-    console.log(messages);
-    return (
-        <FlashMessagesContext.Provider value={{
-            messages,
-            addFlashMessage,
-            removeMessage
-        }}>
-            {children}
-        </FlashMessagesContext.Provider>
-    )
-};
-export default FlashMessagesContextProvider;
-
-export function useFlashMessages() {
-    return useContext(FlashMessagesContext);
-}
 
 
