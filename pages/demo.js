b2c20322b22f7a5b94226362780b727765b84e5d
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2020-09-25 15:42:52 +0200

    fix

--- pages/demo.js
+++ pages/demo.js
@@ -75,9 +75,9 @@ export default function DemoPage() {
 
     const Item = SortableElement(({item}) => {
         return <div className="first:col-span-2 first:row-span-2 flex relative cursor-move focus-visible:underline group">
-            <div className="absolute w-full h-full z-0 rounded-md inset-0 group-hover:opacity-25">
-                <img className="inline-block rounded-md"
-                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            <div className="absolute w-full h-full z-0 rounded-md inset-0 group-hover:opacity-25 bg-green-200 text-center">
+                <img className="inline-block rounded-md p-1 relative"
+                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=200&q=80"
                      alt=""/>
             </div>
             <div className={`z-10 self-start justify-self-auto absolute p-1 text-red ${!item.checked && 'opacity-0' } group-hover:opacity-100 transition duration-500 ease-in-out`}>
@@ -120,12 +120,12 @@ export default function DemoPage() {
                             return <Item key={item.uuid} index={key} item={item}/>
                         })}
                         <div className="group text-center flex relative border-dashed border-2 rounded-md hover:border-gray-400 transition duration-150 ease-in-out ">
-                        <span className="text-gray-500 group-hover:text-gray-600 mx-auto font-medium leading-none p-2 text-sm text-center focus:outline-none focus:underline cursor-pointer">
-                             <svg className="mx-auto text-center h-12 w-12 text-gray-300 group-hover:text-gray-400 " stroke="currentColor" fill="none" viewBox="0 0 48 48">
-                                <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg>
-                            <span className="font-normal block text-xs">Seleccione o  suelte las imágenes aquí </span>
-                        </span>
+                            <span className="text-gray-500 group-hover:text-gray-600 mx-auto self-center font-medium leading-none p-2 text-sm text-center focus:outline-none focus:underline cursor-pointer">
+                                 <svg className="mx-auto text-center h-12 w-12 text-gray-300 group-hover:text-gray-400 " stroke="currentColor" fill="none" viewBox="0 0 48 48">
+                                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                </svg>
+                                <span className="hidden sm:block font-normal block text-xs">Seleccione o  suelte las imágenes aquí </span>
+                            </span>
                         </div>
                     </div>
                 </div>
